20fa42a36e4bd47bef41fa5e65fe74d4149bb87c
tanvirch0750 <tanvir0750@yahoo.com>
2021-10-05 19:00:30 +0600

    Implemented smooth scrolling

--- js/script.js
+++ js/script.js
@@ -11,6 +11,35 @@ buttonEl.addEventListener('click', function() {
    headerEl.classList.toggle('nav-open')
 });
 
+// SMOOTH SCROLLING
+const allLinks = document.querySelectorAll('a:link');
+
+allLinks.forEach(function(link){
+   link.addEventListener('click', function(e) {
+      e.preventDefault();
+      const href = link.getAttribute('href');
+
+      // scroll back to top
+      if(href === '#'){
+         window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+         })
+      }
+      // others link
+      if(href !== '#' && href.startsWith('#')) {
+         const sectionEl = document.querySelector(href);
+         sectionEl.scrollIntoView({
+            behavior: 'smooth',
+         })
+      }
+      // close the mobile navigation
+      if(link.classList.contains('main-nav-link')){
+         headerEl.classList.toggle('nav-open')
+      }
+   }) 
+})
+
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
